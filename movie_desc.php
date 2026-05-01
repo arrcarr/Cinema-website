df007b265a66992014677a528d0d57fd296b4bc1
arrcarr <rafaelfranciscarrillo@gmail.com>
2026-05-01 13:06:15 +0800

    commit 2

--- movie_desc.php
+++ movie_desc.php
@@ -1,22 +1,24 @@
+<?php 
+    include "header.php"; 
+    include "conn.php";
+    $movie = $_SERVER['QUERY_STRING'];
+
+    $sql = "SELECT * FROM tb_movie_table where movie_id=" . $movie . "";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $row['title'] ?></title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 
 <body class="bg-black">
-    <?php 
-        include "header.php"; 
-        include "conn.php";
-        $movie = $_SERVER['QUERY_STRING'];
 
-        $sql = "SELECT * FROM tb_movie_table where movie_id=" . $movie . "";
-        $result = $conn->query($sql);
-        $row = $result->fetch_assoc();
-    ?>
 
     <div class="container text-white" style="background-image: url(<?php echo $row['poster']?>); height:50vh; background-position:center; background-size:cover;">
         <h1 class="fw-bold"> <?php echo $row['title']?> </h1>
